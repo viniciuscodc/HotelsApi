31d843e2becc5330876df972737d83442b95c262
viniciuscodc <viniciuscodc@gmail.com>
2021-07-30 20:04:21 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Serilog;
+using Serilog.Events;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace HotelsApi
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.Console(restrictedToMinimumLevel: LogEventLevel.Information)
+                .WriteTo.File(
+                path: "/home/vinicius/Documents/Projects/HotelsApi/Logs/log-.txt",
+                outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level:u3}] {Message:lj}{NewLine}{Exception}",
+                rollingInterval: RollingInterval.Day,
+                restrictedToMinimumLevel: LogEventLevel.Information
+                ).CreateLogger();
+                try
+                {
+                    Log.Information("Application is starting");
+                    CreateHostBuilder(args).Build().Run();
+                }
+                catch (Exception ex)
+                {
+                    Log.Fatal(ex, "Application failed to start");
+                    throw;
+                }
+                finally{
+                    Log.CloseAndFlush();
+                }
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .UseSerilog()
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
