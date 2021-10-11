f44cc7189fb29e16989d962e94e0fa7c882ab355
viniciuscodc <viniciuscodc@gmail.com>
2021-10-11 18:04:43 -0300

    Removed weather files.

--- Startup.cs
+++ Startup.cs
@@ -26,10 +26,6 @@ namespace HotelsApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-   
-            // services.AddDbContext<DatabaseContext>(options =>
-            //     options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"))
-            // );
 
             services.AddEntityFrameworkNpgsql().AddDbContext<DatabaseContext>(options =>
                 options.UseNpgsql(Configuration.GetConnectionString("DefaultConnection"))
